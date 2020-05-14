5df201a370fddab4a2b982da436c71206ee86685
Darian Miller <darian@darianmiller.com>
2020-05-14 17:49:08 -0500

    Strip module header comment

--- Delphi Tests/Source/rtl/common/System.Generics.Collections/TThreadedQueue/UnitTestQueue/iaUnitTest.TThreadedQueue.dpr
+++ Delphi Tests/Source/rtl/common/System.Generics.Collections/TThreadedQueue/UnitTestQueue/iaUnitTest.TThreadedQueue.dpr
@@ -1,33 +1,4 @@
-(*-----------------------------------------------------------------------------------------------------------------------
- Ideas Awakened, Inc ("COMPANY") CONFIDENTIAL
-
- Unpublished Copyright (c) 2019 Ideas Awakened Inc, All Rights Reserved.
-
- NOTICE:  All information contained herein is and remains the property of COMPANY. The intellectual and technical
- concepts contained herein are proprietary to COMPANY and may be covered by U.S. and Foreign Patents, patents in
- process, and are protected by trade secret or copyright law.
-
- Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
- permission is obtained from COMPANY.
-
- Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees, managers
- or contractors who have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
-
- The copyright notice above does not evidence any actual or intended publication or disclosure of this source code,
- which includes information that is confidential and/or proprietary, and is a trade secret, of COMPANY.
-
- ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE
- CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND
- INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR
- IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT
- MAY DESCRIBE, IN WHOLE OR IN PART.
-
- Ideas Awakened Inc is a privately held company headquartered in Geneseo, Illinois, USA.
- More information is available online at https://www.ideasawakend.com
- Copyright notice version 1.1 2019.12.29 {5A0254F2-D0A6-4059-A77C-C7AEDF3044BA}
------------------------------------------------------------------------------------------------------------------------
-Reminder, command line parameters to minimize output and remove input prompt:  -b -exit:Continue
-*)
+//Reminder, command line parameters to minimize output and remove input prompt:  -b -exit:Continue
 program iaUnitTest.TThreadedQueue;
 
 {$IFNDEF TESTINSIGHT}
