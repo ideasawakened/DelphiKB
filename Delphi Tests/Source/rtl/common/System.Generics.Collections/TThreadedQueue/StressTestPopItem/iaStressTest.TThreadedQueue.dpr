37164400cac033bf504664027930e40a4adefba1
Darian Miller <darian@darianmiller.com>
2020-05-19 10:45:47 -0500

    +Trap exceptions on test execution

--- Delphi Tests/Source/rtl/common/System.Generics.Collections/TThreadedQueue/StressTestPopItem/iaStressTest.TThreadedQueue.dpr
+++ Delphi Tests/Source/rtl/common/System.Generics.Collections/TThreadedQueue/StressTestPopItem/iaStressTest.TThreadedQueue.dpr
@@ -3,13 +3,23 @@ program iaStressTest.TThreadedQueue;
 {$APPTYPE CONSOLE}
 
 uses
+  System.SysUtils,
   iaStressTest.TThreadedQueue.PopItem in 'iaStressTest.TThreadedQueue.PopItem.pas',
-  iaTestSupport.Log in '..\..\..\..\..\iaTestSupport.Log.pas';
+  iaTestSupport.Log in '..\..\..\..\..\iaTestSupport.Log.pas',
+  MonitorWaitStackFix in 'MonitorWaitStackFix.pas';
 
 begin
-  if not StressTestPopItem() then
-  begin
-    ExitCode := 1;
+
+  try
+    if not StressTestPopItem() then
+    begin
+      ExitCode := 1;
+    end;
+  except on E: Exception do
+    begin
+      LogIt('Test failed due to exception: ' + E.Message);
+      ExitCode := 2;
+    end;
   end;
   LogIt('Hit <enter> to exit');
   ReadLn;
