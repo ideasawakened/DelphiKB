cf0a4e87ee2f65dc6aacf0f1cc7495d24082b6b9
Darian Miller <darian@darianmiller.com>
2020-08-31 19:59:27 -0500

    FizzBuzz example project for blog article: https://www.ideasawakened.com/post/developer-interview-questions-fizzbuzz-in-delphi

---
+++ Delphi Demos/FizzBuzz/FizzBuzz.dpr
@@ -0,0 +1,46 @@
+(*
+This doesn't state the strength of a candidate, rather it quickly flags the weak ones.
+
+Write a program in five minutes or less which outputs the string representation of numbers from 1 to 100.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+For numbers which are multiples of both three and five output “FizzBuzz”.
+
+Example:
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+etc...
+
+*)
+program FizzBuzz;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  FizzBuzzExamples in 'FizzBuzzExamples.pas';
+
+begin
+  try
+    FizzBuzzExample4(3,5,1,100);
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  ReadLn;
+end.
